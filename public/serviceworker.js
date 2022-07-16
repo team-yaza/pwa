8ec5d45644af3051d3e99b9abf322cd53ba36b5f
hyunjin <leehj0110@kakao.com>
2022-07-16 18:47:14 +0900

    feat: service worker header

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -9,8 +9,20 @@
 //   }
 // });
 
+// self.addEventListener("fetch", function (event) {
+//   if (event.request.url.includes("/img/logo.png")) {
+//     event.respondWith(fetch("/img/logo-flipped.png"));
+//   }
+// });
+
 self.addEventListener("fetch", function (event) {
-  if (event.request.url.includes("/img/logo.png")) {
-    event.respondWith(fetch("/img/logo-flipped.png"));
-  }
+  event.respondWith(
+    fetch(event.request).catch(function () {
+      return new Response(
+        "Welcome to the Gotham Imperial Hotel.\n" +
+          "There seems to be a problem with your connection. \n" +
+          "We look forward to telling you about our hotel as soon as you go online."
+      );
+    })
+  );
 });
