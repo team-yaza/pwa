a1d7be2680aafff061b008cb4055b0bb80635974
hyunjin <leehj0110@kakao.com>
2022-07-16 17:55:14 +0900

    feat: service worker fetch request

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,13 +1,3 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("/sw.js")
-    .then(function (registration) {
-      console.log(
-        "ServiceWorker registration successful with scope: ",
-        registration.scope
-      );
-    })
-    .catch(function (err) {
-      console.log("ServiceWorker registration failed: ", err);
-    });
-}
+self.addEventListener("fetch", function (event) {
+  console.log("Fetch request for:", event.request.url);
+});
