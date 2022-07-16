d56a6d4f5e458661bcd8d8277a04b9b348b8ad2c
hyunjin <leehj0110@kakao.com>
2022-07-16 18:19:02 +0900

    feat: service worker proxy

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,3 +1,16 @@
+// self.addEventListener("fetch", function (event) {
+//   if (event.request.url.includes("bootstrap.min.css")) {
+//     event.respondWith(
+//       new Response(
+//         ".hotel-slogan {background: green !important;} nav {display: none}",
+//         { headers: { "Content-Type": "text/css" } }
+//       )
+//     );
+//   }
+// });
+
 self.addEventListener("fetch", function (event) {
-  console.log("Fetch request for:", event.request.url);
+  if (event.request.url.includes("/img/logo.png")) {
+    event.respondWith(fetch("/img/logo-flipped.png"));
+  }
 });
